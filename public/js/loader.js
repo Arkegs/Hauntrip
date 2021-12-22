55165d339ff2be9a0ad9b2ad2df45e89329f5a15
Giovanni Orsi <silvestre1602@gmail.com>
2021-12-21 21:18:12 -0300

    improve mystery display

--- public/js/loader.js
+++ public/js/loader.js
@@ -12,20 +12,27 @@ document.getElementById("loadEvidences").addEventListener("click", function(){
                 var fetchedEvidences = data.evidences;
             }
             if(fetchedEvidences.length < 1){
-                document.getElementById('evidences-container').innerHTML += ('<div>No more evidences to load</div>');
+                document.getElementById('evidences-container').innerHTML += ('<div class="loadButton">No more evidences to load</div>');
                 document.getElementById("loadEvidences").style.display = "none"
             } else{
                 console.log(data.currentUser);
                 for(let evidence of fetchedEvidences){
                     let imgString = '';
                     let delString = '';
+                    let helpful = '';
+                    let helpfulMsg = '';
                     for (let evidenceImg of evidence.images){ 
                         imgString+= '<img class="img-thumbnail evidence-thumbnail" src="'+ evidenceImg.url +'">'
                     }
                     if(data.currentUser  === evidence.author._id){
                         delString+=  '<form action="/mysteries/'+ mystery._id +'/evidences/'+ evidence._id+'?_method=DELETE" method="POST"><button class="btn btn-sm btn-danger">Delete</button></form>'
                     }
-                    document.getElementById('evidences-container').innerHTML += ('<div class="card mb-3"><div class="card-body"><h3>' + evidence.title + '</h3><h6 class="card-subtitle mb-2 text-muted">By <a href="/user/'+ evidence.author.username +'">'+ evidence.author.username +' </a></h6><p>Conclusion: '+ evidence.conclusion +' </p><p>Evidence: '+ evidence.body +'</p>' + imgString + delString + '<form action="/mysteries/'+ mystery._id +'/evidences/'+ evidence._id +'" method="POST">             <button type="submit" name="helpfulness" value="1" class="btn btn-sm btn-primary">Upvote</button></form>'+ evidence.helpfulness +'<form action="/mysteries/'+ mystery._id +'/evidences/'+evidence._id+'" method="POST"><button type="submit" name="helpfulness" value="-1" class="btn btn-sm btn-success">Downvote</button></form></div></div>');
+                    if(evidence.helpfulness >= 5){
+                        helpful = 'helpful'
+                        helpfulMsg = '<p class="helpfulMsg">This evidence is considered Helpful!</p>'
+                    }
+                    //document.getElementById('evidences-container').innerHTML += ('<div class="card mb-3"><div class="card-body"><h3>' + evidence.title + '</h3><h6 class="card-subtitle mb-2 text-muted">By <a href="/user/'+ evidence.author.username +'">'+ evidence.author.username +' </a></h6><p>Conclusion: '+ evidence.conclusion +' </p><p>Evidence: '+ evidence.body +'</p>' + imgString + delString + '<form action="/mysteries/'+ mystery._id +'/evidences/'+ evidence._id +'" method="POST">             <button type="submit" name="helpfulness" value="1" class="btn btn-sm btn-primary">Upvote</button></form>'+ evidence.helpfulness +'<form action="/mysteries/'+ mystery._id +'/evidences/'+evidence._id+'" method="POST"><button type="submit" name="helpfulness" value="-1" class="btn btn-sm btn-success">Downvote</button></form></div></div>');
+                    document.getElementById('evidences-container').innerHTML += ('<div class="single-evidence '+ helpful +'"><h4>'+ evidence.title +'</h4><h6 class="author-name">By <a href="/user/'+ evidence.author.username +'">'+ evidence.author.username+'</a></h6><p>Conclusion: '+ evidence.conclusion +'</p><p>Evidence: '+ evidence.body +'</p>' + imgString + '<div class="evidence-panel"><div class="evidence-upvote"><form action="/mysteries/' + mystery._id + '/evidences/'+ evidence._id + '" method="POST"><button type="submit" name="helpfulness" value="1" class="upvote-arrow"></button></form><span class="vote-counter">'+ evidence.helpfulness +'</span><form action="/mysteries/'+ mystery._id +'/evidences/'+ evidence._id +'" method="POST"><button type="submit" name="helpfulness" value="-1" class="downvote-arrow"></button></form></div>' + delString +'</div>' + helpfulMsg + '</div>');
                 }
             }
         })
@@ -34,3 +41,4 @@ document.getElementById("loadEvidences").addEventListener("click", function(){
         });
     loads++;
 });
+
